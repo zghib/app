da077a80d727985ca960fbf02181746d0c93914e
rijkvanzanten <rijkvanzanten@me.com>
2018-04-20 20:20:41 -0400

    7.0.0-alpha.1

---
+++ static/config_example.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+(function directusConfig() {
+  const config = {
+    api: {
+      'https://demo-api.getdirectus.com': 'Directus Demo API'
+    },
+    allowOtherAPI: false
+  };
+
+  window.__DirectusConfig__ = config;
+}());
