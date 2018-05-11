5ba166f264171a87589a1dea1b4e8f9d1240cd5e
rijkvanzanten <rijkvanzanten@me.com>
2018-05-11 17:38:14 -0400

    7.0.0-alpha.3

---
+++ precache-manifest.afe42d66374d5918eebb926df1d838db.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/material-icons.012cf6a1.woff"
+  },
+  {
+    "revision": "10c1a92e4b44c3375bd1a8fc632da014",
+    "url": "/style.css"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/material-icons.570eb838.woff2"
+  },
+  {
+    "revision": "7285316b95d844ed247c",
+    "url": "/js/lang-nl-NL.0.7285316b.js"
+  },
+  {
+    "revision": "18cfe332a1fb14d4b269",
+    "url": "/js/manifest.18cfe332.js"
+  },
+  {
+    "revision": "0089608f2ab8aaa41aba3154051f2e57",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "7e897319d1d3cd0ee674",
+    "url": "/js/vendor.7e897319.js"
+  },
+  {
+    "revision": "555f58376175ec0d3bed77c33890f1ba",
+    "url": "/img/logo.555f5837.svg"
+  },
+  {
+    "revision": "c777111a72470a17bcb5",
+    "url": "/js/app.c777111a.js"
+  },
+  {
+    "revision": "41641057565f67ad18735d17ca35cdc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "713f50cf19db27736ee3d58713ac4f54",
+    "url": "/img/box.713f50cf.svg"
+  },
+  {
+    "revision": "fb63a2a5e055c7162236c32f956c2c1b",
+    "url": "/fonts/roboto-regular.fb63a2a5.woff2"
+  },
+  {
+    "revision": "17ee45df7466d24f5a2cd5cfe3ed761a",
+    "url": "/fonts/roboto-regular.17ee45df.woff"
+  },
+  {
+    "revision": "4c104b963611ee56cf683b9d7a27617d",
+    "url": "/fonts/roboto-medium.4c104b96.woff2"
+  },
+  {
+    "revision": "0b13ce4c83dff3f78559239cfc3f7f4d",
+    "url": "/fonts/roboto-medium.0b13ce4c.woff"
+  },
+  {
+    "revision": "316e008b8f3adf89ab74b1b5ed0c0427",
+    "url": "/fonts/roboto-bold.316e008b.woff"
+  },
+  {
+    "revision": "136c5ca09f682932ac7ff909eeebda31",
+    "url": "/fonts/roboto-bold.136c5ca0.woff2"
+  },
+  {
+    "revision": "c777111a72470a17bcb5",
+    "url": "/css/app.7871ad3a.css"
+  },
+  {
+    "revision": "be2f6cee10f74bb71f8a531d533e1c58",
+    "url": "/config_example.js"
+  }
+];
