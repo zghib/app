b54cc87f356b9333e012af3d6f49d32ecf76df72
rijkvanzanten <rijkvanzanten@me.com>
2018-05-18 23:17:13 -0400

    v7.0.0-alpha.4

---
+++ precache-manifest.3d0e67570150497d68b7248dbaa3763f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/material-icons.012cf6a1.woff"
+  },
+  {
+    "revision": "10c1a92e4b44c3375bd1a8fc632da014",
+    "url": "/style.css"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/material-icons.570eb838.woff2"
+  },
+  {
+    "revision": "7285316b95d844ed247c",
+    "url": "/js/lang-nl-NL.0.7285316b.js"
+  },
+  {
+    "revision": "18cfe332a1fb14d4b269",
+    "url": "/js/manifest.18cfe332.js"
+  },
+  {
+    "revision": "97a76d257a5f9d2687a9",
+    "url": "/js/vendor.97a76d25.js"
+  },
+  {
+    "revision": "0b13ce4c83dff3f78559239cfc3f7f4d",
+    "url": "/fonts/roboto-medium.0b13ce4c.woff"
+  },
+  {
+    "revision": "18315f865d6b150b6b25",
+    "url": "/js/app.18315f86.js"
+  },
+  {
+    "revision": "c6642319f54d7f1cbfdb5aa50b9d392b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555f58376175ec0d3bed77c33890f1ba",
+    "url": "/img/logo.555f5837.svg"
+  },
+  {
+    "revision": "713f50cf19db27736ee3d58713ac4f54",
+    "url": "/img/box.713f50cf.svg"
+  },
+  {
+    "revision": "fb63a2a5e055c7162236c32f956c2c1b",
+    "url": "/fonts/roboto-regular.fb63a2a5.woff2"
+  },
+  {
+    "revision": "17ee45df7466d24f5a2cd5cfe3ed761a",
+    "url": "/fonts/roboto-regular.17ee45df.woff"
+  },
+  {
+    "revision": "4c104b963611ee56cf683b9d7a27617d",
+    "url": "/fonts/roboto-medium.4c104b96.woff2"
+  },
+  {
+    "revision": "316e008b8f3adf89ab74b1b5ed0c0427",
+    "url": "/fonts/roboto-bold.316e008b.woff"
+  },
+  {
+    "revision": "136c5ca09f682932ac7ff909eeebda31",
+    "url": "/fonts/roboto-bold.136c5ca0.woff2"
+  },
+  {
+    "revision": "18315f865d6b150b6b25",
+    "url": "/css/app.c65725ea.css"
+  },
+  {
+    "revision": "be2f6cee10f74bb71f8a531d533e1c58",
+    "url": "/config_example.js"
+  }
+];
