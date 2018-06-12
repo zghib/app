26318dffaea5301e6760fa165069dfa01264739c
rijkvanzanten <rijkvanzanten@me.com>
2018-06-12 09:52:02 -0400

    v7.0.0-alpha.6

---
+++ precache-manifest.b118882f4ee60d9939cac81bec556e97.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b13ce4c83dff3f78559239cfc3f7f4d",
+    "url": "/fonts/roboto-medium.0b13ce4c.woff"
+  },
+  {
+    "revision": "10c1a92e4b44c3375bd1a8fc632da014",
+    "url": "/style.css"
+  },
+  {
+    "revision": "17ee45df7466d24f5a2cd5cfe3ed761a",
+    "url": "/fonts/roboto-regular.17ee45df.woff"
+  },
+  {
+    "revision": "e28ddbab36cb4c645740",
+    "url": "/js/lang-nl-NL.e28ddbab.js"
+  },
+  {
+    "revision": "484310736b8c3fd1a6998b2078ef1c67",
+    "url": "/script.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/material-icons.012cf6a1.woff"
+  },
+  {
+    "revision": "4c104b963611ee56cf683b9d7a27617d",
+    "url": "/fonts/roboto-medium.4c104b96.woff2"
+  },
+  {
+    "revision": "fb63a2a5e055c7162236c32f956c2c1b",
+    "url": "/fonts/roboto-regular.fb63a2a5.woff2"
+  },
+  {
+    "revision": "fd39fbe860f5c75b4a9e8c655b05b042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a897ac591ee95663caed",
+    "url": "/js/chunk-vendors.a897ac59.js"
+  },
+  {
+    "revision": "fd1a61281b834080d824",
+    "url": "/js/app.fd1a6128.js"
+  },
+  {
+    "revision": "713f50cf19db27736ee3d58713ac4f54",
+    "url": "/img/box.713f50cf.svg"
+  },
+  {
+    "revision": "555f58376175ec0d3bed77c33890f1ba",
+    "url": "/img/logo.555f5837.svg"
+  },
+  {
+    "revision": "316e008b8f3adf89ab74b1b5ed0c0427",
+    "url": "/fonts/roboto-bold.316e008b.woff"
+  },
+  {
+    "revision": "136c5ca09f682932ac7ff909eeebda31",
+    "url": "/fonts/roboto-bold.136c5ca0.woff2"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/material-icons.570eb838.woff2"
+  },
+  {
+    "revision": "fd1a61281b834080d824",
+    "url": "/css/app.0e016875.css"
+  },
+  {
+    "revision": "c97664a751c36899c518aa6db8ad67be",
+    "url": "/config_example.js"
+  }
+];
