0149a227a1d66ebed3b22d241be2bf6bbeebeb81
rijkvanzanten <rijkvanzanten@me.com>
2018-07-09 23:03:06 +0200

    v7.0.0-beta.1

---
+++ precache-manifest.07d05db8be186e326c364383a21fa8de.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd6783f3c6654d48ad2f6e27b1b64bb8",
+    "url": "/fonts/roboto-light.fd6783f3.woff2"
+  },
+  {
+    "revision": "10c1a92e4b44c3375bd1a8fc632da014",
+    "url": "/style.css"
+  },
+  {
+    "revision": "0b13ce4c83dff3f78559239cfc3f7f4d",
+    "url": "/fonts/roboto-medium.0b13ce4c.woff"
+  },
+  {
+    "revision": "e28ddbab36cb4c645740",
+    "url": "/js/lang-nl-NL.e28ddbab.js"
+  },
+  {
+    "revision": "484310736b8c3fd1a6998b2078ef1c67",
+    "url": "/script.js"
+  },
+  {
+    "revision": "17ee45df7466d24f5a2cd5cfe3ed761a",
+    "url": "/fonts/roboto-regular.17ee45df.woff"
+  },
+  {
+    "revision": "c2b94f49a45b5fe5a35e3d2e73745ff7",
+    "url": "/fonts/roboto-light.c2b94f49.woff"
+  },
+  {
+    "revision": "2a78cc292fc57e93661295491068fee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb63a2a5e055c7162236c32f956c2c1b",
+    "url": "/fonts/roboto-regular.fb63a2a5.woff2"
+  },
+  {
+    "revision": "713f50cf19db27736ee3d58713ac4f54",
+    "url": "/img/box.713f50cf.svg"
+  },
+  {
+    "revision": "f507a07b613659ed9488",
+    "url": "/js/chunk-vendors.f507a07b.js"
+  },
+  {
+    "revision": "2a3e559a2d8809750e55",
+    "url": "/js/app.2a3e559a.js"
+  },
+  {
+    "revision": "4c104b963611ee56cf683b9d7a27617d",
+    "url": "/fonts/roboto-medium.4c104b96.woff2"
+  },
+  {
+    "revision": "661fa73cb7e377d8885e0ddbe8950d8b",
+    "url": "/img/sprite.661fa73c.svg"
+  },
+  {
+    "revision": "316e008b8f3adf89ab74b1b5ed0c0427",
+    "url": "/fonts/roboto-bold.316e008b.woff"
+  },
+  {
+    "revision": "136c5ca09f682932ac7ff909eeebda31",
+    "url": "/fonts/roboto-bold.136c5ca0.woff2"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/material-icons.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/material-icons.012cf6a1.woff"
+  },
+  {
+    "revision": "2a3e559a2d8809750e55",
+    "url": "/css/app.8e6874df.css"
+  },
+  {
+    "revision": "dab278e9b85c260b5d70953c802595bd",
+    "url": "/config_example.js"
+  }
+];
