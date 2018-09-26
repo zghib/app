d2af4152f545860cf0ba615773eee55e9a4498c3
Ben Haynes <ben@rngr.org>
2018-09-26 11:30:16 -0400

    Add vue.config.js for theme override

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pwa: {
+    name: 'Directus',
+    themeColor: '#263238',
+    msTileColor: '#263238',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+  }
+}
