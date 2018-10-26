2d77c5cf414fc6804a96ca8eabfe81f9d39d8053
rijkvanzanten <rijkvanzanten@me.com>
2018-10-26 10:50:15 -0400

    Safely ready php version if it exists
    Closes #1021

--- src/store/modules/server-info/actions.js
+++ src/store/modules/server-info/actions.js
@@ -1,5 +1,6 @@
 import api from "../../../api";
 import { SET_SERVER_INFO } from "../../mutation-types";
+import { get } from "lodash";
 
 export function getServerInfo({ commit }) {
   api
@@ -7,9 +8,9 @@ export function getServerInfo({ commit }) {
     .then(res => res.data)
     .then(info => {
       commit(SET_SERVER_INFO, {
-        apiVersion: info.api.version,
-        phpVersion: info.server.general.php_version,
-        maxUploadSize: info.server.max_upload_size
+        apiVersion: get(info, "api.version"),
+        phpVersion: get(info, "server.general.php_version"),
+        maxUploadSize: get(info, "server.max_upload_size")
       });
     });
 }
