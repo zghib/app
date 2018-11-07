dd75270d251dd0d864744cc7082049bfec314877
rijkvanzanten <rijkvanzanten@me.com>
2018-11-07 15:37:53 -0500

    Fix comments tab not activating on no comments
    Closes #1086

--- src/components/activity/activity.vue
+++ src/components/activity/activity.vue
@@ -125,6 +125,8 @@ export default {
         revision: this.revisions[activity.id]
       }));
 
+      if (activityWithChanges.length === 0) return [];
+
       const lastItem =
         activityWithChanges &&
         activityWithChanges[activityWithChanges.length - 1];
