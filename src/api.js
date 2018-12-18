67cdee843197ac073a9db2a1362a233d16d54b4d
rijkvanzanten <rijkvanzanten@me.com>
2018-12-18 18:17:31 -0500

    Move auth info to store on load

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import SDK from "@directus/sdk-js";
 import store from "./store";
+import { LOGIN_SUCCESS } from "./store/mutation-types";
 
 const client = new SDK({
   storage: window.sessionStorage
@@ -8,5 +9,13 @@ const client = new SDK({
 client.onAutoRefreshError = function logout(error) {
   store.dispatch("logout", error);
 };
+
+if (client.loggedIn) {
+  store.commit(LOGIN_SUCCESS, {
+    project: client.project,
+    token: client.token,
+    url: client.url
+  });
+}
 
 export default client;
