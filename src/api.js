ca306fec632e4bde374911cc85002c18e048e8d6
rijkvanzanten <rijkvanzanten@me.com>
2018-12-19 16:14:19 -0500

    Fix project name on refresh

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 import SDK from "@directus/sdk-js";
+import formatTitle from "@directus/format-title";
+import _ from "lodash";
 import store from "./store";
 import { LOGIN_SUCCESS } from "./store/mutation-types";
 
@@ -11,11 +13,34 @@ client.onAutoRefreshError = function logout(error) {
 };
 
 if (client.loggedIn) {
+  const config = window.__DirectusConfig__; // eslint-disable-line
+  const urls = config
+    ? _.mapKeys(config.api, (val, key) => (key.endsWith("/") ? key : key + "/"))
+    : null;
+
   store.commit(LOGIN_SUCCESS, {
     project: client.project,
     token: client.token,
-    url: client.url
+    url: client.url,
+    projectName:
+      urls[client] ||
+      urls[client.url + "/" + client.project + "/"] ||
+      formatTitle(extractHostname(client.url))
   });
 }
 
 export default client;
+
+function extractHostname(url) {
+  let hostname;
+
+  if (url.indexOf("://") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+  hostname = hostname.split(":")[0];
+  hostname = hostname.split("?")[0];
+
+  return hostname;
+}
