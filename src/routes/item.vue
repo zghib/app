5474518bf76180a65bcf1f4cd509373117a78ad4
Rijk van Zanten <rijkvanzanten@me.com>
2019-02-18 11:11:45 -0500

    Cast correct type of default value for array type
    Closes #1328

--- src/routes/item.vue
+++ src/routes/item.vue
@@ -374,7 +374,13 @@ export default {
       return this.$store.state.collections[this.collection];
     },
     defaultValues() {
-      return this.$lodash.mapValues(this.fields, field => field.default_value);
+      return this.$lodash.mapValues(this.fields, field => {
+        if (field.type === "array") {
+          return [field.default_value];
+        }
+
+        return field.default_value;
+      });
     },
     values() {
       const edits = this.$store.state.edits.values;
