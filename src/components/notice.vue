8facfd7732de7a80eb742d5985f1fffa2e69f553
Ben Haynes <ben@rngr.org>
2019-04-13 20:27:49 -0400

    add notice component

---
+++ src/components/notice.vue
@@ -0,0 +1,36 @@
+<template>
+  <p class="notice" :class="color">
+    <slot />
+  </p>
+</template>
+
+<script>
+export default {
+  name: "v-notice",
+  props: {
+    color: {
+      type: String,
+      default: "gray",
+      validator(val) {
+        return ["gray", "accent", "warning", "danger", "success"].includes(val);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.notice {
+  position: relative;
+  padding: 16px;
+  border-radius: var(--border-radius);
+  max-width: 630px;
+  &.gray {
+    background-color: var(--lightest-gray);
+  }
+  &.warning {
+    color: var(--warning);
+    background-color: var(--warning-light);
+  }
+}
+</style>
