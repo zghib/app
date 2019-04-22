c5f0dcf37b49d1a64d5898bcc18b7b23c72644ba
Rijk van Zanten <rijkvanzanten@me.com>
2019-04-22 09:40:18 -0400

    Don't inject prefetch headers
    This would cause the browser to prefetch a ton of tiny chunks (
    interfaces, layouts, etc) which can cause rate limiters to kick in,
    bricking the application. Most of these chunks are never needed in the
    first place (no need to download every language file f.e.)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? "" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "" : "/",
+
+  // There are so many chunks (from all the interfaces / layouts) that we need to make sure to not
+  // prefetch them all. Prefetching them all will cause the server to apply rate limits in most cases
+  chainWebpack: config => {
+    config.plugins.delete("prefetch");
+  }
 };
