ebbfd3b1be1e6a621dee7a5a96d897caa7ca96b9
Nitwel <nitwel@arcor.de>
2019-05-23 21:27:35 +0200

    "Interfaces" Interfaces (#1668)
    * - fixed click events
    
    * - added interfaces interface into seperate PR

---
+++ src/interfaces/interfaces/input.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-select
+    :value="value"
+    :disabled="readonly"
+    :id="name"
+    :options="choices"
+    :placeholder="options.placeholder"
+    @input="$emit('input', $event)"
+  ></v-select>
+</template>
+
+<script>
+import mixin from "@directus/extension-toolkit/mixins/interface";
+
+export default {
+  mixins: [mixin],
+  computed: {
+    choices() {
+      const interfaces = this.$store.state.extensions.interfaces || {};
+
+      let choices = {};
+
+      Object.keys(interfaces).forEach(key => {
+        choices[key] = this.$helpers.formatTitle(key);
+      });
+
+      return choices;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-select {
+  margin-top: 0;
+  max-width: var(--width-medium);
+}
+</style>
