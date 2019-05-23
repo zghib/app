ddc1e5fbcd4c1def1f4de1ffe3da860eefa3a9b3
christianrr <10274479+christianrr@users.noreply.github.com>
2019-05-23 17:09:01 +0200

    Updated & added missing german translations [de-DE] (#1654)
    * Updated & added missing translations in de-DE
    
    * Fixed double quotes in german translation [de-DE]
    
    Co-Authored-By: Rijk van Zanten <rijkvanzanten@me.com>
    
    * Fixed german translations [de-DE]

--- src/lang/locales/de-DE.js
+++ src/lang/locales/de-DE.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 export const dateTimeFormats = {
   short: {
     year: "numeric",
@@ -18,6 +20,7 @@ export const dateTimeFormats = {
 export default {
   about_directus: "Über Directus",
   activity: "Aktivität",
+  my_activity: "Meine Aktivität",
   activity_log: "Aktivitäten-Log",
   activity_outside_directus: "Dieses Element wurde außerhalb von Directus erstellt.",
   admin_email: "Admin-E-Mail",
@@ -27,13 +30,14 @@ export default {
   add_field_filter: "Neuen Feld-Filter hinzufügen",
   add_note: "Füge eine hilfreiche Beschreibung für die Benutzer an...",
   admin_settings: "Admin-Einstellungen",
-  almost_done_options: "Fast geschaft! Bitte überprüfe unten die Feldeinstellungen.",
+  almost_done_options: "Fast fertig! Bitte überprüfe unten die Feldeinstellungen.",
+  advanced_options: "Erweiterte Optionen",
   almost_done_copy:
     "Jeder Feldtyp kann perfekt an deine Bedürfnisse angepasst werden. Nachfolgend die verfügbaren Optionen für das Feld, aber nur ein paar sind erforderlich.",
   all: "Alle",
-  statuses: "Status",
   api_url: "API URL",
   api_installed: "API erfolgreich installiert",
+  auto_generate: "Auto-Generierung",
   auto_generated: "Automatisch generiert...",
   batch: "Batch",
   batch_edit: "Batch-Bearbeitung",
@@ -43,15 +47,21 @@ export default {
   between: "Zwischen",
   both: "Beide",
   bookmarks: "Lesezeichen",
+  bookmark_global: "Global: Alle Benutzer",
+  bookmark_personal: "Persönlich: Nur meine",
+  bookmark_role: "Rolle: {role}",
+  bookmark_select_visibility: "Sichtbarkeit des Lesezeichens auswählen:",
   cancel: "Abbrechen",
   change_project: "Projekt wechseln",
   choose_one: "Bitte wählen",
   choose_project: "Projekt auswählen",
   choose_interface: "Wähle einen Feldtyp über den die Daten gepflegt werden sollen.",
+  clear: "Löschen",
   click_to_toggle_all_none: "Klicken um die ganze Spalte ein- und auszublenden",
   connection: "Verbindung",
   collection: "Sammlung",
   collections_and_fields: "Sammlungen & Felder",
+  collection_invalid_name: "Ungültiger Name für die Sammlung",
   collection_updated: "Sammlung {collection} aktualisiert",
   collection_removed: "Sammlung {collection} entfernt",
   collection_names_cannot_be_changed:
@@ -64,6 +74,10 @@ export default {
   comment: "Kommentar",
   comments: "Kommentare",
   coming_soon: "Bald verfügbar",
+  compact: "Klein",
+  cozy: "Mittel",
+  comfortable: "Groß",
+  confirm: "Confirm",
   contains: "Enthält",
   config_error: "Keine App-Konfiguration vorhanden",
   config_error_copy: "Bitte stelle sicher, dass Du die Konfigurationsdatei der App erstellt hast.",
@@ -71,28 +85,108 @@ export default {
   currently_selected: "Aktuelle Auswahl: {thing}",
   create: "Erstellen",
   creating_item: "Element wird erstellt",
+  creating_item_page_title: "Element wird erstellt: {collection}",
   creating_role: "Rolle wird erstellt",
   create_role: "Rolle erstellen",
   create_field: "Feld erstellen",
+  update_field: "Feld wird aktualisiert",
   create_collection: "Sammlung erstellen",
+  created_by: "Erstellt von",
+  created_on: "Erstellt am",
   database_connection: "Datenbankverbindung",
   database_connection_copy:
     "Wie soll die Datenbankverbindung hergestellt werden, in der dieses Projekt gepflegt wird.",
   datatype: "Datentyp",
+  db_datatype: "{db} Datentyp",
+  editing_my_profile: "Mein Profil bearbeiten",
+  embed: "Einbetten",
+  embed_placeholder: "YouTube, Vimeo, oder anderer Link",
+  fieldtypes: {
+    alias: "Felder, die keine Daten speichern oder eine entsprechende Datenbankspalte verfügen",
+    array: "Standard Array-Format in API-Response",
+    datetime: "Datum und Uhrzeit im ISO format, z.B.: 2018-09-19T14:00:43.381Z",
+    date: "Datum, z.B.: 2018-09-19",
+    time: "Uhrzeit, z.B.: 14:09:22",
+    file: "Fremdschlüssel zu directus_files.id",
+    group:
+      "Gruppenfelder sind visuell zusammengefasst, Kinder speichern die Gruppe in directus_fields.group",
+    integer: "Ganze Zahl",
+    decimal: "Gleitkommazahl",
+    json: "Standard JSON-Format in API-Response",
+    lang: "Spezifisch für Übersetzungen; dies speichert den Verweis auf die Sprache",
+    m2o: "n:1-Beziehung",
+    o2m: "1:n-Beziehung",
+    managed: "Verwaltet",
+    sort: "Systemfeld, das für Drag&Drop-Umordnungen verwendet wird",
+    status: "Systemfeld, das für das Veröffentlichen von Datensätzen verwendet wird",
+    string:
+      "Beliebiger Text, Zahlen, Leerzeichen oder Symbole; Eingeschränkt durch die Länge (Anzahl der Zeichen)",
+    translation: "Spezifisch für Übersetzungen; dies speichert mehrsprachige Inhalte",
+    uuid: "Eine universell eindeutige ID",
+    datetime_created:
+      "Systemfeld, um den Zeitpunkt des Erstellens eines Elements zu verfolgen; wird für Revisionen verwendet",
+    datetime_updated:
+      "Systemfeld, um den Zeitpunkt des Bearbeitens eines Elements zu verfolgen; wird für Revisionen verwendet",
+    user_created:
+      "Systemfeld, um den Ersteller eines Elements zu verfolgen; wird für Revisionen verwendet",
+    user_updated:
+      "Systemfeld, um den Bearbeiter eines Elements zu verfolgen; wird für Revisionen verwendet"
+  },
+  datatypes: {
+    mysql: {
+      CHAR:
+        "Ein nicht-binärer String (fixe Länge, mit Leerzeichen nach rechts aufgefüllt) Länge von 0 bis 255 Zeichen.",
+      VARCHAR:
+        "Ein nicht-binärer String (variable Länge). Länge von 0 to 65,535 Zeichen (MySQL 5.0.3+).",
+      TINYTEXT: "Ein String mit einer maximalen Länge von 255 Zeichen.",
+      TEXT: "in String mit einer maximalen Länge von 65,535 Zeichen.",
+      MEDIUMTEXT: "Ein String mit einer maximalen Länge von 16,777,215 Zeichen.",
+      LONGTEXT: "in String mit einer maximalen Länge von 4,294,967,295 Zeichen.",
+
+      BOOLEAN: "Wahr oder falsch, gespeichert als entweder 1 oder 0.",
+      TINYINT:
+        "Ein Integer. Wenn vorzeichenbehaftet (negative Zahlen erlaubt) Länge von -128 bis 127, sonst maximal 255.",
+      SMALLINT:
+        "Ein Integer. Wenn vorzeichenbehaftet (negative Zahlen erlaubt) Länge von -32,768 bis 32,767, sonst maximal 65,535.",
+      MEDIUMINT:
+        "Ein Integer. Wenn vorzeichenbehaftet (negative Zahlen erlaubt) Länge von -8,388,608 bis 8,388,607, sonst maximal 16,777,215.",
+      INT:
+        "Ein Integer. Wenn vorzeichenbehaftet (negative Zahlen erlaubt) Länge von -2,147,483,648 bis 2,147,483,647, sonst maximal 4,294,967,295.",
+      BIGINT:
+        "Ein Integer. Wenn vorzeichenbehaftet (negative Zahlen erlaubt) Länge von -9,223,372,036,854,775,808 bis 9,223,372,036,854,775,807, sonst maximal 18,446,744,073,709,551,615.",
+
+      DECIMAL:
+        "Genaue Gleitkommazahl (z.B. für Währungen), mit 1 bis 65 Vorkommastellen (L) and 0 bis 30 Nachkommastellen (D). Format: `L,D`",
+      FLOAT: "Gleitkommazahl, mit 4 Bytes und einem variablen Dezimalpunkt.",
+      DOUBLE: "Gleitkommazahl, mit 8 Bytes und einem variablen Dezimalpunkt.",
+
+      DATE: "Datum. Erlaubt ist `1000-01-01` bis `9999-12-31`.",
+      DATETIME: "Datum und Uhrzeit. Erlaubt ist `1000-01-01 00:00:00` bis `9999-12-31 23:59:59`.",
+      TIMESTAMP:
+        "Datum und Uhrzeit. Erlaubt ist `1970-01-01 00:00:01` UTC bis `2038-01-19 03:14:07` UTC.",
+      TIME: "Zeit. Erlaubt ist `-838:59:59` bis `838:59:59`.",
+      YEAR: "Jahr. Erlaubt ist `1901` bis `2155` or `0000`"
+    }
+  },
+  date_and_time: "Datum und Uhrzeit",
   db_user: "Benutzer (Datenbank)",
   db_password: "Benutzerpasswort (Datenbank)",
   db_name: "Name in der Datenbank",
   db_type: "Datenbanktyp",
+  db_column_name: "Datenbankfeldname",
+  db_updated: "Datenbank erfolgreich aktualisiert",
+  db_update_failed: "Datenbank konnte nicht aktualisiert werden.",
   default: "Standard",
+  default_value: "Standardwert",
   delete: "Löschen",
   delete_are_you_sure:
-    "Soll dieses Element wirklich gelöscht werden?? Diese Aktion kann nicht rückgängig gemacht werden.",
+    "Soll dieses Element wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden.",
   delete_collection_are_you_sure:
-    "Soll diese Sammlung wirklich gelöscht werden?? Diese Aktion kann nicht rückgängig gemacht werden.",
+    "Soll diese Sammlung wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden.",
   delete_field_are_you_sure:
-    'Soll das Feld "{field}" wirklich gelöscht werden?? Diese Aktion kann nicht rückgängig gemacht werden.',
+    'Soll das Feld "{field}" wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden.',
   delete_role_are_you_sure:
-    'Soll die Rolle "{name}" wirklich gelöscht werden?? Diese Aktion kann nicht rückgängig gemacht werden.',
+    'Soll die Rolle "{name}" wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden.',
   delete_bookmark: "Lesezeichen löschen",
   delete_bookmark_body:
     "Soll das Lesezeichen wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden.",
@@ -102,6 +196,7 @@ export default {
   discard_changes: "Änderungen verwerfen",
   display_name: "Anzeigename",
   drop_files: "Dateien hier hineinziehen...",
+  done: "Erledigt",
   dont_show: "Nicht anzeigen",
   editing: "{collection} werden bearbeitet",
   editing_item: "Element wird bearbeitet",
@@ -116,6 +211,7 @@ export default {
   enter_collection_name: "Namen der Sammlung eingeben...",
   enter_role_name: "Rollennamen eingeben...",
   environment: "Umgebung",
+  project_key: "Projektschlüssel",
   errors: {
     "-1": "API kann nicht erreicht werden",
     11: "Keine Datenbankverbindung",
@@ -123,7 +219,8 @@ export default {
     101: "Aufgrund von Inaktivität abgemeldet",
     102: "Aufgrund von Inaktivität abgemeldet",
     103: "Benutzer inaktiv",
-    106: "Falscher Benutzername und/oder Passwort"
+    106: "Falscher Benutzername und/oder Passwort",
+    107: "Benutzer nicht gefunden"
   },
   error_unknown: "Unbekannter Fehler. Bitte versuche es später noch einmal",
   esc_cancel: "Escape: Abbrechen und Fenster schließen",
@@ -131,10 +228,12 @@ export default {
   existing: "vorhanden",
   fetching_data: "Daten werden abgerufen",
   field: "Feld",
+  field_type: "Feldtyp",
   field_created: "Feld {field} erstellt",
   field_updated: "Feld {field} aktualisiert",
   field_removed: "Feld {field} gelöscht",
   field_already_exists: "{field} Ist bereits vorhanden",
+  field_width: "Feldbreite",
   fields: "Felder",
   fields_are_saved_automatically: "Feldeinstellungen werden automatisch gespeichert",
   file: "Datei",
@@ -145,9 +244,11 @@ export default {
   greater_than: "Größer als",
   help_and_docs: "Hilfe & Dokumentation",
   keep_editing: "Weiter bearbeiten",
-  hidden_detail: "In Detailansicht verstecken",
-  hidden_browse: "In Listenansicht verstecken",
+  hidden: "Ausgeblendet",
+  hidden_detail: "In Detailansicht ausblenden",
+  hidden_browse: "In Listenansicht ausblenden",
   host: "Host",
+  icon: "Icon",
   in_list: "Eines von diesen",
   initial_schema: "Initiales Schema wählen",
   initial_schema_copy:
@@ -174,6 +275,10 @@ export default {
   item_deleted: "Element gelöscht",
   item_saved: "Element gespeichert | {count} Elemente gespeicherte",
   extension_error: "Es ist ein Problem beim Laden der {ext} Erweiterung aufgetreten.",
+  junction_collection: "Verbindungssammlung",
+  cant_disable_primary:
+    "Die Primärschlüsseleigenschaft kann nicht deaktiviert werden. Feld muss gelöscht werden.",
+  cant_sort_by_this_field: "Nach diesem Feld kann nicht sortiert werden",
   extensions_missing: "Keine Erweiterungen gefunden",
   extensions_missing_copy:
     "Bitte stelle sicher, dass Du die System-Erweiterungen installiert hast.",
@@ -181,6 +286,7 @@ export default {
   learn_more: "Mehr erfahren",
   leave_comment: "Kommentieren...",
   length: "Länge",
+  length_disabled_placeholder: "Länge ist durch Datentyp definiert",
   less_than_equal: "Weniger oder gleich",
   less_than: "Weniger als",
   limited: "Beschränkt",
@@ -189,6 +295,8 @@ export default {
   login: "Login",
   m2m: "m:n-Beziehung (M2M)",
   m2o: "n:1-Beziehung (M2O)",
+  modified_by: "Geändert durch",
+  modified_on: "Geändert am",
   o2m: "1:n-Beziehung (O2M)",
   name: "Name",
   name_field: "Gib diesem {field}-Feld einen Namen und konfiguriere es.",
@@ -196,10 +304,18 @@ export default {
     "Bist Du sicher, dass Du diese Seite verlassen möchtest? Alle Änderungen gehen dabei verloren.",
   new: "Neu",
   new_file: "Neue Datei",
+  new_field: "Neues Feld",
   no_related_entries: "Keine ähnlichen Einträge",
   not_between: "Nicht zwischen",
   not_contains: "Enthält nicht",
   note: "Notiz",
+  note_note: "Eine interne Beschreibung...",
+  note_hidden: "[Mehr erfahren](https://docs.directus.io/guides/collections.html#hidden)",
+  note_icon: "Icon in der Navigation-Sidebar der App",
+  note_managed:
+    "[Mehr erfahren](https://docs.directus.io/guides/collections.html#managing-collections)",
+  note_single: "[Mehr erfahren](https://docs.directus.io/guides/collections.html#single)",
+  notifications: "Benachrichtigungen",
   no_results: "Keine Ergebnisse",
   no_results_body: "Kein Element bei den aktuellen Filtereinstellungen",
   no_collections: "Keine Sammlungen",
@@ -217,7 +333,13 @@ export default {
   not_empty: "Ist nicht leer",
   not_equal_to: "Ist nicht gleich mit",
   not_in_list: "Keines von diesen",
+  no_interface_error: "Feld {field} verfügt über keine Schnittstelle",
+  optional: "Optional",
   manage: "Verwalten",
+  manage_started: "Verwaltung von {collection} begonnen",
+  manage_stopped: "Verwaltung von {collection} beendet",
+  max_size: "Max. Größe: {size}",
+  max_one_primary_key: "Es ist nur ein Primärschlüssel pro Sammlung erlaubt",
   dont_manage: "Nicht verwalten",
   dont_manage_copy:
     "Berechtigungen und Einstellungen der Sammlung {collection} werden endgültig gelöscht! Bist Du sicher?",
@@ -227,9 +349,13 @@ export default {
   other: "Andere",
   ok: "OK",
   operator: "Anwender",
+  open_on_gh: "Auf GitHub öffnen",
   options: "Optionen",
   password: "Passwort",
   password_reset_sent: "Eine E-Mail zum Zurücksetzen des Passworts wurde an {email} gesendet",
+  permissions_no_collections: "Dieses Projekt verfügt noch über keine Sammlungen.",
+  permissions_admin:
+    "Administratoren haben standardmäßig die Berechtigung alle Daten im System zu verändern.",
   permission_states: {
     none: "Keine",
     mine: "Nur meine",
@@ -242,8 +368,10 @@ export default {
     on_update: "Beim Aktualisieren",
     always: "Immer"
   },
+  popular: "Häufig genutzt",
   powered_by_directus: "Powered by Directus",
   preview_and_revert: "Vorschau und Zurücksetzen",
+  primary_key: "Primärschlüssel",
   port: "Port",
   project: "Projekt",
   project_name: "Projektname",
@@ -254,15 +382,22 @@ export default {
   readable_fields: "Lesbare Felder",
   readable_fields_copy: "Wähle die Felder aus, die der User sehen kann",
   readonly: "Lesezugriff",
+  relational: "Relational",
+  report_issue: "Problem bekanntgeben",
+  request_feature: "Feature anfragen",
   regex: "RegEx",
+  relation_setup:
+    "Da dies ein relationales Feld ist, müssen die verknüpften Felder definiert werden.",
+  relation_setup_copy:
+    "Es gibt verschiedene Arten von relationalen Daten, dieses Feld verwendet die Abhängigkeit {relation}.",
   related_entries: "Ähnliche Einträge vorhanden",
+  related_collection: "Verknüpfte Sammlung",
   remove: "Löschen",
   remove_related: "Ähnlichen Eintrag löschen",
   relationship: "Beziehung",
   relationship_setup: "Für dieses Beziehungsfeld müssen die verknüpften Felder abgebildet werden.",
   relationship_setup_copy:
     "Es gibt unterschiedliche Type relationaler Daten. Dieses Feld benutzt: {relationship}",
-  related_collection: "Verknüpfte Sammlung",
   required: "Erforderlich",
   reset_preferences: "Alle Listeneinstellungen zurücksetzen",
   reset_password: "Passwort zurücksetzen",
@@ -288,29 +423,43 @@ export default {
   select_statuses: "Status auswählen",
   select_statuses_copy: "Status auswählen, die der Benutzer verwenden darf",
   settings: "Einstellungen",
-  settings_interface: "Interface-Einstellungen",
+  settings_interface: "Feldtyp-Einstellungen",
+  settings_project: "Projekt-Einstellungen",
   settings_global: "Globale Einstellungen",
+  settings_update_database: "Datenbank aktualisieren",
+  settings_update_database_subtext: "Skript ausführen",
+  settings_update_database_confirm:
+    "Dies wird ihr Datenbankschema auf die letzte Version aktualisieren. Bevor diese Operation gestartet wird sollte ein Backup erstellt werden.",
   settings_collections_fields: "Sammlungen & Felder",
   settings_permissions: "Rollen & Berechtigungen",
   settings_extensions: "Erweiterungen",
+  settings_saved: "Einstellungen gespeichert",
   server_details: "Serverdetails",
   server_error: "Serverfehler",
   server_error_copy: "Es ist ein Problem mit dem Server oder der Datenbank aufgetreten.",
   server_trouble: "Serverproblem",
   server_trouble_copy: "Versuche es später noch einmal oder kontaktiere den Systemadministrator.",
-  show_advanced_options: "Erweiterte Optionen anzeigen",
   show_directus_collections: "Interne System-Sammlungen anzeigen",
+  single: "Einzeln",
+  show_advanced_options: "Erweiterte Optionen anzeigen",
   something_went_wrong: "Irgendwas ist schief gelaufen...",
   something_went_wrong_body:
     "Wir haben ein Problem bei der Verarbeitung deiner Anfrage... Bitte versuche es noch mal durch Neuladen dieser Seite. Wenn das Problem weiterhin besteht, kontaktiere bitte deinen Systemadministrator.",
+  sort: "Sortieren",
+  sort_by: "Sortieren Nach",
+  spacing: "Abstand",
   sign_in: "Login",
   signing_in: "Einloggen",
   sign_out: "Ausloggen",
   sign_out_confirm: "Bist Du sicher, dass Du Dich ausloggen möchtest?",
   sign_out_confirm_edits:
     "Bist Du sicher, dass Du Dich ausloggen möchtest? Alle ungespeicherten Änderungen gehen dabei verloren.",
-  to: "An",
+  status: "Status",
+  statuses: "Status",
+  submit: "Absenden",
+  text: "Text",
   this_collection: "Diese Sammlung",
+  to: "An",
   unsaved_changes_copy:
     "Es gibt ungespeicherte Änderungen! Bist Du sicher, dass Du diese Seite verlassen möchtest?",
   user_directory: "Benutzerverzeichnis",
@@ -325,9 +474,15 @@ export default {
   version: "Version",
   version_and_updates: "Version und Aktualisierungen",
   view_type: "Ansehen als...",
+  visible_all_users: "Für alle Benutzer sichtbar",
+  welcome: "Willkommen",
   writable_fields: "Beschreibbare Felder",
   write_fields_copy: "Wähle die Felder aus, die der User bearbeiten darf.",
   yes: "Ja",
   duplicating_field: "Feld duplizieren",
-  duplicate: "Duplikat"
+  duplicate: "Duplikat",
+  upload_exceeds_max_size:
+    "{filename} konnte nicht hochgeladen werden. Die Serverkonfiguration erlaubt Uploads dieser Größe nicht.",
+  file_type_not_accepted:
+    "{filename} konnte nicht hochgeladen werden. Der Dateityp ist für dieses Feld nicht erlaubt."
 };
