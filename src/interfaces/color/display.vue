c8ae10efc0b3fb06731fd4d3cc892cde895994f9
Rijk van Zanten <rijkvanzanten@me.com>
2020-01-22 17:32:34 -0500

    Render color swatch for color interface (#2458)
    Fixes #2457

--- src/interfaces/color/display.vue
+++ src/interfaces/color/display.vue
@@ -1,12 +1,25 @@
-<template><p>Color</p></template>
+<template>
+	<div v-tooltip="value" class="swatch" :style="{ backgroundColor: value }"></div>
+</template>
 
 <script>
 import mixin from '@directus/extension-toolkit/mixins/interface';
-// import Color from "color";
 
 export default {
 	mixins: [mixin]
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.swatch {
+	transition: var(--fast) var(--transition);
+	display: inline-block;
+	width: 24px;
+	height: 24px;
+	border-radius: 100%;
+	vertical-align: middle;
+	margin-right: 4px;
+	color: var(--white);
+	text-align: center;
+}
+</style>
