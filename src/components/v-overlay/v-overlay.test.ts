a6c8e2af540f4ec6812682aef3db22b325061fe0
Rijk van Zanten <rijkvanzanten@me.com>
2020-01-22 17:28:03 -0500

    Add overlay component (#2455)

---
+++ src/components/v-overlay/v-overlay.test.ts
@@ -0,0 +1,47 @@
+import { mount, createLocalVue, Wrapper } from '@vue/test-utils';
+import VueCompositionAPI from '@vue/composition-api';
+
+const localVue = createLocalVue();
+localVue.use(VueCompositionAPI);
+
+import VOverlay from './v-overlay.vue';
+
+describe('Overlay', () => {
+	let component: Wrapper<Vue>;
+
+	beforeEach(() => {
+		component = mount(VOverlay, {
+			localVue
+		});
+	});
+
+	it('Is invisible when active prop is false', () => {
+		expect(component.isVisible()).toBe(false);
+	});
+
+	it('Is visible when active is true', async () => {
+		component.setProps({ active: true });
+		await component.vm.$nextTick();
+		expect(component.isVisible()).toBe(true);
+	});
+
+	it('Sets position absolute based on absolute prop', async () => {
+		component.setProps({ active: true, absolute: true });
+		await component.vm.$nextTick();
+		expect(component.classes()).toContain('absolute');
+	});
+
+	it('Sets the inline styles based on props', async () => {
+		component.setProps({
+			active: true,
+			absolute: true,
+			color: '--red',
+			zIndex: 50,
+			opacity: 0.2
+		});
+		await component.vm.$nextTick();
+		expect((component.vm as any).styles['--_v-overlay-color']).toEqual('var(--red)');
+		expect((component.vm as any).styles['--_v-overlay-z-index']).toEqual(50);
+		expect((component.vm as any).styles['--_v-overlay-opacity']).toEqual(0.2);
+	});
+});
---
+++ src/components/v-overlay/v-overlay.vue
@@ -0,0 +1,82 @@
+<template>
+	<div class="v-overlay" v-show="active" :class="{ active, absolute }" :style="styles">
+		<div class="overlay" />
+		<div v-if="active" class="content"><slot /></div>
+	</div>
+</template>
+
+<script lang="ts">
+import { createComponent, computed } from '@vue/composition-api';
+import parseCSSVar from '@/utils/parse-css-var';
+
+export default createComponent({
+	props: {
+		active: {
+			type: Boolean,
+			default: false
+		},
+		absolute: {
+			type: Boolean,
+			default: false
+		},
+		color: {
+			type: String,
+			default: '--modal-smoke-color'
+		},
+		zIndex: {
+			type: Number,
+			default: 500
+		},
+		opacity: {
+			type: Number,
+			default: 0.75
+		}
+	},
+	setup(props) {
+		const styles = computed(() => ({
+			'--_v-overlay-color': parseCSSVar(props.color),
+			'--_v-overlay-z-index': props.zIndex,
+			'--_v-overlay-opacity': props.opacity
+		}));
+
+		return { styles };
+	}
+});
+</script>
+
+<style lang="scss">
+.v-overlay {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	pointer-events: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: var(--_v-overlay-z-index);
+
+	&.active {
+		pointer-events: auto;
+	}
+
+	&.absolute {
+		position: absolute;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: var(--_v-overlay-color);
+		opacity: var(--_v-overlay-opacity);
+	}
+
+	.content {
+		position: relative;
+	}
+}
+</style>
