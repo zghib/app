438d7a976f34d25266e38e068b95be1fed6eaac5
rijkvanzanten <rijkvanzanten@me.com>
2020-02-03 16:27:12 -0500

    Add mobile on / off states for nav / drawer

--- src/views/private/private.vue
+++ src/views/private/private.vue
@@ -6,10 +6,12 @@
 		</aside>
 		<header class="header">
 			<button @click="navOpen = true">Toggle nav</button>
+			<button @click="drawerOpen = true">Toggle drawer</button>
 		</header>
 		<main class="content"></main>
 		<aside class="drawer" :class="{ 'is-open': drawerOpen }"></aside>
-		<v-overlay :active="navOpen" :z-index="5" @click="navOpen = false" />
+		<v-overlay :active="navOpen" :z-index="49" @click="navOpen = false" />
+		<v-overlay :active="drawerOpen" :z-index="24" @click="drawerOpen = false" />
 	</div>
 </template>
 
@@ -41,7 +43,8 @@ export default createComponent({
 		position: fixed;
 		top: 0;
 		left: 0;
-		z-index: 10;
+		z-index: 50;
+		transition: transform var(--slow) var(--transition);
 
 		&.is-open {
 			transform: translateX(0);
@@ -61,6 +64,22 @@ export default createComponent({
 			height: 100%;
 			display: inline-block;
 			font-size: 1rem;
+		}
+	}
+
+	.drawer {
+		width: 284px;
+		height: 100%;
+		background-color: #eceff1;
+		position: fixed;
+		top: 0;
+		right: 0;
+		transform: translateX(100%);
+		transition: transform var(--slow) var(--transition);
+		z-index: 25;
+
+		&.is-open {
+			transform: translateX(0);
 		}
 	}
 }
