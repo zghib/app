b2ab8c04bc99f2dcd24298d365386db7c57a0170
Rijk van Zanten <rijkvanzanten@me.com>
2020-02-07 15:06:21 -0500

    Fix confirm modal cancel button styling (#2582)

--- src/components/modals/confirm.vue
+++ src/components/modals/confirm.vue
@@ -1,9 +1,17 @@
 <template>
 	<v-modal-base :title="title" :message="message" @cancel="$emit('cancel')">
 		<div class="buttons" @keydown.esc="$emit('cancel')">
-			<button class="cancel" @click="$emit('cancel')">
+			<v-button
+				class="cancel"
+				outlined
+				background-color="--button-tertiary-background-color"
+				hover-background-color="--button-tertiary-background-color"
+				color="--button-tertiary-text-color"
+				hover-color="--button-tertiary-text-color-hover"
+				@click="$emit('cancel')"
+			>
 				{{ cancelText || $t('cancel') }}
-			</button>
+			</v-button>
 			<v-button :background-color="`--${color}`" class="confirm" @click="$emit('confirm')">
 				<template v-if="loading">
 					<v-spinner :size="20" :line-size="2" />
@@ -62,27 +70,6 @@ export default {
 }
 
 .cancel {
-	border: none;
-	border-radius: var(--border-radius);
-	cursor: pointer;
-	transition: var(--fast) var(--transition);
-	transition-property: border-color, background-color, color;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	padding: 0 20px 1px;
-	height: var(--button-height);
-	min-width: 160px;
-	border: var(--input-border-width) solid var(--button-tertiary-background-color);
-	// background-color: var(--button-secondary-background-color);
-	color: var(--button-tertiary-text-color);
-	margin-right: 20px;
-	font-weight: var(--weight-bold);
-	font-size: var(--button-font-size);
-	transition: color var(--fast) var(--transition);
-
-	&:hover {
-		color: var(--button-tertiary-text-color-hover);
-	}
+	margin-right: 12px;
 }
 </style>
