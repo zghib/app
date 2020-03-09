ece161a7443ed3f1e673518e2aa90e439321e6f7
Nicolas Turlais <nicolas.turlais@gmail.com>
2020-03-09 19:21:04 +0100

    Use v-if to avoid rendering too many icons. fix #2588 (#2645)
    * Use v-if to avoid rendering too many icons. fix #2588
    
    * Removed unused variable
    
    * Tweak styling of section headers
    
    Co-authored-by: rijkvanzanten <rijkvanzanten@me.com>

--- src/interfaces/icon/input.vue
+++ src/interfaces/icon/input.vue
@@ -9,9 +9,14 @@
 			icon-left="search"
 		></v-input>
 		<div v-show="searchText.length === 0" class="icons-view">
-			<details v-for="(icongroup, groupname) in icons" :key="groupname" open>
+			<details
+				v-for="(icongroup, groupname) in icons"
+				:key="groupname"
+				@toggle="handleIconGroupToggle(groupname)"
+				:open="openIconGroups[groupname] ? 'open' : undefined"
+			>
 				<summary>{{ $helpers.formatTitle(groupname) }}</summary>
-				<div>
+				<div v-if="openIconGroups[groupname]">
 					<button
 						v-for="icon in icongroup"
 						:key="icon"
@@ -50,7 +55,8 @@ export default {
 	mixins: [mixin],
 	data() {
 		return {
-			searchText: ''
+			searchText: '',
+			openIconGroups: {}
 		};
 	},
 	computed: {
@@ -62,6 +68,11 @@ export default {
 		},
 		filteredArray() {
 			return this.iconsArray.filter(icon => icon.includes(this.searchText.toLowerCase()));
+		}
+	},
+	methods: {
+		handleIconGroupToggle(groupname) {
+			this.$set(this.openIconGroups, groupname, !this.openIconGroups[groupname]);
 		}
 	}
 };
@@ -93,6 +104,14 @@ export default {
 		summary {
 			margin: 20px 2px 5px;
 			cursor: pointer;
+			color: var(--modal-tab-text-color);
+
+			&:hover {
+				color: var(--page-text-color);
+			}
+		}
+
+		&[open] summary {
 			color: var(--page-text-color);
 		}
 	}
