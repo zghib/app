28e16507885225b9929566add78a8f99ac117ea0
Ion Zghibarta <ion.zghibarta@ellation.com>
2020-05-08 09:15:12 +0300

    Add new Status-field as dropdown

---
+++ src/interfaces/status_dropdown/display.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="no-wrap">
+		{{ currentStatus.name }}
+	</div>
+</template>
+
+<script>
+import mixin from '@directus/extension-toolkit/mixins/interface';
+
+export default {
+	mixins: [mixin],
+	computed: {
+		currentStatus() {
+			return this.options.status_mapping[this.value];
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped></style>
